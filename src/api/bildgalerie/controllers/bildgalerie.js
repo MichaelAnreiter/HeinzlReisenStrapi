c49f5a3904e639a8d0a089073ca4b808f49b3624
Michael Anreiter <anreitermichael1@gmail.com>
2025-02-21 16:46:55 +0100

    updates - Strapi Content

---
+++ src/api/bildgalerie/controllers/bildgalerie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bildgalerie controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::bildgalerie.bildgalerie');
